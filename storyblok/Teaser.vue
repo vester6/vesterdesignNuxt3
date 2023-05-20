a08e36599b4026f24546d59e62a05b283c4344f3
vester6 <mave@tv2.dk>
2023-05-20 17:37:30 +0200

    1 file modified

--- storyblok/Teaser.vue
+++ storyblok/Teaser.vue
@@ -3,10 +3,12 @@
     v-editable="blok"
     :cat="$attrs.cat"
     class="py-8 mb-6 text-5xl font-bold text-center"
+    style="red"
     data-test="teaser"
   >
-    {{ blok.headline }}
+    {{ blok.headline }} 
   </div>
+  <p>test</p>
 </template>
 
 <script setup>
